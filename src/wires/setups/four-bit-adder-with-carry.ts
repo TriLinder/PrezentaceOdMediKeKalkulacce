0a9ca62ee487a5af02cfb5ff36d4643ae343e30f
TriLinder <82846064+TriLinder@users.noreply.github.com>
2023-05-22 15:30:29 +0200

    Add wires to four bit adder setup

--- src/wires/setups/four-bit-adder-with-carry.ts
+++ src/wires/setups/four-bit-adder-with-carry.ts
@@ -23,6 +23,12 @@ export class FourBitAdderWithCarrySetup extends WireSetup {
         const adderXOffset = 325
         const adderYOffset = -400
 
+        const secondNumberToAdderCableCornerOffsetX = -300;
+        const secondNumberToAdderCableCornerOffsetY = -300;
+
+        const adderToSumOutputCableCornerOffsetX = 300;
+        const adderToSumOutputCableCornerOffsetY = 300;
+
         //Inputs
         const firstNumberInputAComponent = new SwitchComponent(new Coordinates(0, 120), new Size(ioComponentSize, ioComponentSize), [new Power(true)]);
         const firstNumberInputBComponent = new SwitchComponent(new Coordinates(0, firstNumberInputAComponent.getSnapPoint("bottom").y + ioComponentGap), new Size(ioComponentSize, ioComponentSize), [new Power(true)]);
@@ -48,7 +54,43 @@ export class FourBitAdderWithCarrySetup extends WireSetup {
         const sumOutputCComponent = new LampComponent(new Coordinates(3840 - ioComponentSize, sumOutputBComponent.getSnapPoint("bottom").y + ioComponentGap), new Size(ioComponentSize, ioComponentSize), [secondAdderComponent.getOutput(0)]);
         const sumOutputDComponent = new LampComponent(new Coordinates(3840 - ioComponentSize, sumOutputCComponent.getSnapPoint("bottom").y + ioComponentGap), new Size(ioComponentSize, ioComponentSize), [firstAdderComponent.getOutput(0)]);
 
-        const carryOutputComponent = new LampComponent(new Coordinates(3840 - ioComponentSize, sumOutputDComponent.getSnapPoint("bottom").y + ioCategoryGap), new Size(ioComponentSize, ioComponentSize), [thirdAdderComponent.getOutput(1)]);
+        const carryOutputComponent = new LampComponent(new Coordinates(3840 - ioComponentSize, sumOutputDComponent.getSnapPoint("bottom").y + ioCategoryGap), new Size(ioComponentSize, ioComponentSize), [fourthAdderComponent.getOutput(1)]);
+
+        //Wires
+        const carryInputToFirstAdderWire = new WireComponent(carryInputComponent.getSnapPoint("center"), firstAdderComponent.getSnapPoint("input2"), 25, [carryInputComponent.getOutput(0)]);
+        const firstAddderToSecondAdderWire = new WireComponent(firstAdderComponent.getSnapPoint("output1"), secondAdderComponent.getSnapPoint("input2"), 25, [firstAdderComponent.getOutput(1)]);
+        const secondAddderToThirdAdderWire = new WireComponent(secondAdderComponent.getSnapPoint("output1"), thirdAdderComponent.getSnapPoint("input2"), 25, [secondAdderComponent.getOutput(1)]);
+        const thirdAddderToFourthAdderWire = new WireComponent(thirdAdderComponent.getSnapPoint("output1"), fourthAdderComponent.getSnapPoint("input2"), 25, [thirdAdderComponent.getOutput(1)]);
+        const fourthAdderToCarryOutputWire = new WireComponent(fourthAdderComponent.getSnapPoint("output1"), carryOutputComponent.getSnapPoint("left"), 25, [fourthAdderComponent.getOutput(1)]);
+
+        const firstNumberInputDToFirstAdderWire = new WireComponent(firstNumberInputDComponent.getSnapPoint("center"), firstAdderComponent.getSnapPoint("input0"), 25, [firstNumberInputDComponent.getOutput(0)]);
+        const firstNumberInputCToSecondAdderWire = new WireComponent(firstNumberInputCComponent.getSnapPoint("center"), secondAdderComponent.getSnapPoint("input0"), 25, [firstNumberInputCComponent.getOutput(0)]);
+        const firstNumberInputBToThirdAdderWire = new WireComponent(firstNumberInputBComponent.getSnapPoint("center"), thirdAdderComponent.getSnapPoint("input0"), 25, [firstNumberInputBComponent.getOutput(0)]);
+        const firstNumberInputAToFourthAdderWire = new WireComponent(firstNumberInputAComponent.getSnapPoint("center"), fourthAdderComponent.getSnapPoint("input0"), 25, [firstNumberInputAComponent.getOutput(0)]);
+        
+        const secondNumberInputDToFirstAdderWire1 = new WireComponent(secondNumberInputDComponent.getSnapPoint("center"), new Coordinates(firstAdderComponent.getSnapPoint("input1").x + 0.5 * secondNumberToAdderCableCornerOffsetX, firstAdderComponent.getSnapPoint("input1").y + 0.5 * secondNumberToAdderCableCornerOffsetY), 25, [secondNumberInputDComponent.getOutput(0)]);
+        const secondNumberInputDToFirstAdderWire2 = new WireComponent(secondNumberInputDToFirstAdderWire1.getSnapPoint("end"), firstAdderComponent.getSnapPoint("input1"), 25, [secondNumberInputDComponent.getOutput(0)]);
+        
+        const secondNumberInputCToSecondAdderWire1 = new WireComponent(secondNumberInputCComponent.getSnapPoint("center"), new Coordinates(secondAdderComponent.getSnapPoint("input1").x + secondNumberToAdderCableCornerOffsetX, secondAdderComponent.getSnapPoint("input1").y + secondNumberToAdderCableCornerOffsetY), 25, [secondNumberInputCComponent.getOutput(0)]);
+        const secondNumberInputCToSecondAdderWire2 = new WireComponent(secondNumberInputCToSecondAdderWire1.getSnapPoint("end"), secondAdderComponent.getSnapPoint("input1"), 25, [secondNumberInputCComponent.getOutput(0)]);
+
+        const secondNumberInputBToThirdAdderWire1 = new WireComponent(secondNumberInputBComponent.getSnapPoint("center"), new Coordinates(thirdAdderComponent.getSnapPoint("input1").x + 1.5 * secondNumberToAdderCableCornerOffsetX, thirdAdderComponent.getSnapPoint("input1").y + 2.25 * secondNumberToAdderCableCornerOffsetY), 25, [secondNumberInputBComponent.getOutput(0)]);
+        const secondNumberInputBToThirdAdderWire2 = new WireComponent(secondNumberInputBToThirdAdderWire1.getSnapPoint("end"), thirdAdderComponent.getSnapPoint("input1"), 25, [secondNumberInputBComponent.getOutput(0)]);
+
+        const secondNumberInputAToFourthAdderWire1 = new WireComponent(secondNumberInputAComponent.getSnapPoint("center"), new Coordinates(fourthAdderComponent.getSnapPoint("input1").x + 2.5 * secondNumberToAdderCableCornerOffsetX, fourthAdderComponent.getSnapPoint("input1").y + 2.5 * secondNumberToAdderCableCornerOffsetY), 25, [secondNumberInputAComponent.getOutput(0)]);
+        const secondNumberInputAToFourthAdderWire2 = new WireComponent(secondNumberInputAToFourthAdderWire1.getSnapPoint("end"), fourthAdderComponent.getSnapPoint("input1"), 25, [secondNumberInputAComponent.getOutput(0)]);
+
+        const firstAdderToSumOutputDWire1 = new WireComponent(firstAdderComponent.getSnapPoint("output0"), new Coordinates(firstAdderComponent.getSnapPoint("output0").x + 2.5 * adderToSumOutputCableCornerOffsetX, firstAdderComponent.getSnapPoint("output0").y + 2.5 * adderToSumOutputCableCornerOffsetY), 25, [firstAdderComponent.getOutput(0)]);
+        const firstAdderToSumOutputDWire2 = new WireComponent(firstAdderToSumOutputDWire1.getSnapPoint("end"), sumOutputDComponent.getSnapPoint("left"), 25, [firstAdderComponent.getOutput(0)]);
+
+        const secondAdderToSumOutputCWire1 = new WireComponent(secondAdderComponent.getSnapPoint("output0"), new Coordinates(secondAdderComponent.getSnapPoint("output0").x + 2 * adderToSumOutputCableCornerOffsetX, secondAdderComponent.getSnapPoint("output0").y + 2 * adderToSumOutputCableCornerOffsetY), 25, [secondAdderComponent.getOutput(0)]);
+        const secondAdderToSumOutputCWire2 = new WireComponent(secondAdderToSumOutputCWire1.getSnapPoint("end"), sumOutputCComponent.getSnapPoint("left"), 25, [secondAdderComponent.getOutput(0)]);
+
+        const thirdAdderToSumOutputBWire1 = new WireComponent(thirdAdderComponent.getSnapPoint("output0"), new Coordinates(thirdAdderComponent.getSnapPoint("output0").x + 1.5 * adderToSumOutputCableCornerOffsetX, thirdAdderComponent.getSnapPoint("output0").y + 1.5 * adderToSumOutputCableCornerOffsetY), 25, [thirdAdderComponent.getOutput(0)]);
+        const thirdAdderToSumOutputBWire2 = new WireComponent(thirdAdderToSumOutputBWire1.getSnapPoint("end"), sumOutputBComponent.getSnapPoint("left"), 25, [thirdAdderComponent.getOutput(0)]);
+
+        const fourthAdderToSumOutputAWire1 = new WireComponent(fourthAdderComponent.getSnapPoint("output0"), new Coordinates(fourthAdderComponent.getSnapPoint("output0").x + 0.5 * adderToSumOutputCableCornerOffsetX, fourthAdderComponent.getSnapPoint("output0").y + 0.5 * adderToSumOutputCableCornerOffsetY), 25, [fourthAdderComponent.getOutput(0)]);
+        const fourthAdderToSumOutputAWire2 = new WireComponent(fourthAdderToSumOutputAWire1.getSnapPoint("end"), sumOutputAComponent.getSnapPoint("left"), 25, [fourthAdderComponent.getOutput(0)]);
 
         //Labels
         const firstNumberLabel = new DynamicNumberLabelComponent(new Coordinates(firstNumberInputAComponent.getSnapPoint("left").x, (firstNumberInputAComponent.getSnapPoint("center").y) - 190), 120, [firstNumberInputAComponent.getOutput(0), firstNumberInputBComponent.getOutput(0), firstNumberInputCComponent.getOutput(0), firstNumberInputDComponent.getOutput(0)]);
@@ -58,6 +100,34 @@ export class FourBitAdderWithCarrySetup extends WireSetup {
         const sumNumberLabel = new DynamicNumberLabelComponent(new Coordinates(sumOutputAComponent.getSnapPoint("left").x, (sumOutputAComponent.getSnapPoint("center").y) - 190), 120, [fourthAdderComponent.getOutput(0), thirdAdderComponent.getOutput(0), secondAdderComponent.getOutput(0), firstAdderComponent.getOutput(0)]);
         const carryOutputLabel = new LabelComponent(new Coordinates(carryOutputComponent.getSnapPoint("left").x, (carryOutputComponent.getSnapPoint("center").y) - 190), " ↪ ", 120);
 
+        //Add the wires first, so they're in the background
+        this.addComponent(carryInputToFirstAdderWire);
+        this.addComponent(firstAddderToSecondAdderWire);
+        this.addComponent(secondAddderToThirdAdderWire);
+        this.addComponent(thirdAddderToFourthAdderWire);
+        this.addComponent(fourthAdderToCarryOutputWire);
+        this.addComponent(firstNumberInputDToFirstAdderWire);
+        this.addComponent(firstNumberInputCToSecondAdderWire);
+        this.addComponent(firstNumberInputBToThirdAdderWire);
+        this.addComponent(firstNumberInputAToFourthAdderWire);
+        this.addComponent(secondNumberInputDToFirstAdderWire1);
+        this.addComponent(secondNumberInputDToFirstAdderWire2);
+        this.addComponent(secondNumberInputCToSecondAdderWire1);
+        this.addComponent(secondNumberInputCToSecondAdderWire2);
+        this.addComponent(secondNumberInputBToThirdAdderWire1);
+        this.addComponent(secondNumberInputBToThirdAdderWire2);
+        this.addComponent(secondNumberInputAToFourthAdderWire1);
+        this.addComponent(secondNumberInputAToFourthAdderWire2);
+        this.addComponent(firstAdderToSumOutputDWire1);
+        this.addComponent(firstAdderToSumOutputDWire2);
+        this.addComponent(secondAdderToSumOutputCWire1);
+        this.addComponent(secondAdderToSumOutputCWire2);
+        this.addComponent(thirdAdderToSumOutputBWire1);
+        this.addComponent(thirdAdderToSumOutputBWire2);
+        this.addComponent(fourthAdderToSumOutputAWire1);
+        this.addComponent(fourthAdderToSumOutputAWire2);
+
+        //Now add the actual components
         this.addComponent(firstNumberInputAComponent);
         this.addComponent(firstNumberInputBComponent);
         this.addComponent(firstNumberInputCComponent);
@@ -77,6 +147,7 @@ export class FourBitAdderWithCarrySetup extends WireSetup {
         this.addComponent(thirdAdderComponent);
         this.addComponent(fourthAdderComponent);
 
+        //Add labels last
         this.addComponent(firstNumberLabel);
         this.addComponent(secondNumberLabel);
         this.addComponent(carryInputLabel);
